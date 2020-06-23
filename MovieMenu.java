b9e2c05586e4e2f32935a54b49a1ec0302882afd
Alyssa <50183184+ampettit@users.noreply.github.com>
2020-06-23 00:08:31 -0500

    Update MovieMenu.java

--- MovieMenu.java
+++ MovieMenu.java
@@ -1,9 +1,18 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
 public class MovieMenu {
+	private enum status{
+		RECEIVED, RELEASED
+	}
+	
 	public String cmd;
 	
 	public static void printMenu() {
@@ -31,24 +40,7 @@ public class MovieMenu {
 		break;
 		
 		case 2:
-	/*
-	// add movies  <=== adds movies - prompts input per info needed
-		// Request Title
-			System.out.println("What is the title of the movie you would like to add?");
-			title = scnr.nextLine();
-		// Request Release Date
-			System.out.printf("What is the release date of %s?/nPlease provide date in mm.dd.yyyy format.", title);
-		// >> set released / received based on current date
-			releaseDate = scnr.nextLine();
-		
-		// Request Description
-			System.out.printf("What is a description of %s?/n", title);
-			description = scnr.nextLine();
-		// TODO add code to add the movie
-		
-			System.out.printf("Added %s(%s) releasing on: %s.", title, description, releaseDate);
-	*/
-			Movie.addMovie();
+			MovieMenu.addMovie(scnr);
 			
 		break;
 		
@@ -92,13 +84,73 @@ public class MovieMenu {
 		break;
 	}
 }
+	public static void loadMovieFile(ArrayList<Movie> showing, DoubleLinkedList<Movie> coming) throws FileNotFoundException {
+		int coming_index = 0;
+		int showing_index = 0;
+		
+		// Create file stream to get input file
+		FileInputStream inputFile = new FileInputStream("Movies.txt");
+		// Create scanner to get data from file
+		Scanner inputScanner = new Scanner(inputFile);
+				
+		// Create output file
+		FileOutputStream outputFile = new FileOutputStream("log.txt");
+		// Create print writer to output data to output file
+		PrintWriter writeOutput = new PrintWriter(outputFile);		
+		
+		while (inputScanner.hasNext()) {
+			Movie addMovie = new Movie();
+			addMovie.setMovieTitle(inputScanner.next());
+			addMovie.setMovieTitle(inputScanner.next());
+			try {
+				addMovie.setReleaseDate(new SimpleDateFormat("MM/dd/yyyy").parse(inputScanner.next()));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			addMovie.setDescription(inputScanner.next());
+			try {
+				addMovie.setReceivedDate(new SimpleDateFormat("MM/dd/yyyy").parse(inputScanner.next()));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (inputScanner.next().equalsIgnoreCase(status.RELEASED.toString())) {
+				showing.add(addMovie);
+			} else {
+				coming.addLast(addMovie);
+			}
+		}
+	}
+	public void addMovie(Scanner scnr) throws ParseException{
+		String movieTitle, description;
+		Date releaseDate, receivedDate;
+		String recDate, dateRel = null;	
+		
+		System.out.println("Enter movie title:");
+		movieTitle = scnr.next();
+		System.out.println("Enter release date:");
+		System.out.println("Format: MM/dd/yyyy");
+		releaseDate = new SimpleDateFormat("MM/dd/yyyy").parse(dateRel);
+		System.out.println("Enter description:");
+		description = scnr.next();
+		System.out.println("Enter recieved date");
+		System.out.println("Format: MM/dd/yyyy");
+		recDate = scnr.next();
+		receivedDate = new SimpleDateFormat("MM/dd/yyyy").parse(recDate);
+	}
 	public static void main(String[] args) throws Exception {
 		Scanner scnr = new Scanner(System.in);
 		int userSelected;
 		
+		ArrayList<Movie> showing = new ArrayList<Movie>();
+		DoubleLinkedList<Movie> coming = new DoubleLinkedList<Movie>();
+		
 		System.out.println("Welcome to MovieMaster8000!");
 		System.out.println("What would you like to do? -press 0 to view menu.");
 		userSelected = scnr.nextInt();
+		
+		loadMovieFile(showing, coming);
 		
 		
 		while (userSelected > -1 && userSelected < 8) {

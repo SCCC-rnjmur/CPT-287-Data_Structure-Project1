eed539a31a79b9cd541ae526b87fe64d2489bc96
Alyssa <50183184+ampettit@users.noreply.github.com>
2020-06-23 03:00:33 -0500

    Update MovieMenu.java

--- MovieMenu.java
+++ MovieMenu.java
@@ -1,6 +1,7 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -8,6 +9,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.ListIterator;
 import java.util.Scanner;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class MovieMenu {
 	private enum status{
@@ -18,22 +21,26 @@ public class MovieMenu {
 	
 	public static void printMenu() {
 		System.out.println("What would you like to do?  Choose a number from the menu below.");
-		System.out.println("0....Display Menu");
-		System.out.println("1....Display All Movies");
-		System.out.println("2....Add a new movie");
-		System.out.println("3....Edit Movie Details");
+		System.out.println("0....Display Menu");  // <--- DONE
+		System.out.println("1....Display All Movies"); // <--- DONE
+		System.out.println("2....Add a new movie"); // <--- DONE
+		System.out.println("3....Edit Movie Details"); 
 		System.out.println("4....Change Start Showing Date");
 		System.out.println("5....Count Movies Released Before a Date");
-		System.out.println("6....Save Changes");
-		System.out.println("7....Exit");
-		//System.out.println("8....");
-		
+		System.out.println("6....Save Changes"); // <--- DONE
+		System.out.println("7....Exit\n"); // <--- DONE
 	}
-	public static String menu(int cmd, Scanner scnr, ArrayList<Movie> coming, DoubleLinkedList<Movie> showing) throws Exception{
+	public static String menu(int cmd, Scanner scnr, ArrayList<Movie> showing, DoubleLinkedList<Movie> coming) throws Exception{
 		String userDate;
 		switch(cmd) {
 		case 1:
-			ListIterator comingIter = coming.listIterator();
+			System.out.println("SHOWING NOW");
+			ListIterator showingIter = showing.listIterator();
+			while(showingIter.hasNext()) {
+				System.out.println(showingIter.next().toString());
+			}
+			System.out.println("COMING SOON - To a theater near YOU!");
+			ListIterator comingIter = coming.iterator();
 			while(comingIter.hasNext()) {
 				System.out.println(comingIter.next().toString());
 			}
@@ -44,15 +51,45 @@ public class MovieMenu {
 		break;
 		
 		case 2:
-			addMovie(scnr);
+			addMovie(showing, coming);
 		break;
 		
 		case 3:
-			// edit movie description  <===  ???
-			// list all movies, with numbers 
-			System.out.println("Editing movie description");
-			// edit release date  <=== ???
-			System.out.println("Editing release date");
+			// edit movie description
+			// list all movies
+			comingIter = coming.iterator();
+			int comingCount = 0;
+			System.out.println("What movie would you like to edit?");
+			while(comingIter.hasNext()) {
+				comingCount ++;
+				System.out.printf("%d...." + comingIter.next().toString());
+			}
+			comingIter = coming.iterator();
+			Scanner inputScanner = new Scanner(System.in);
+			int userChoice = inputScanner.nextInt();
+			
+			if (userChoice > comingCount) {
+				System.out.println("That was an invalid option.\n");
+				break;
+			}
+			
+			for (int x = 1; x <= userChoice; x++) {
+				comingIter.next();
+			}
+			System.out.println("Enter new description below.\nIf no changes are needed, leave blank.");
+			String newDescription = inputScanner.nextLine();
+			System.out.println("Enter new release date below.\nIf no changes are needed, leave blank.");
+			Date newReleaseDate = new SimpleDateFormat("MM/dd/yyyy").parse(inputScanner.nextLine());
+			
+			if (newDescription.compareTo("") != 0) {
+				// add set code for description
+			} 
+			if (newReleaseDate.toString().compareTo("") != 0) {
+				// add set code for date
+			}
+			
+			//TODO add set code for release date
+			
 		break;
 		
 		case 4:
@@ -72,12 +109,13 @@ public class MovieMenu {
 		
 		case 6:
 		// save  <=== writes to file
-			//TODO call print to file method
+			writeMovieFile(showing, coming);
 			System.out.println("Your changes have been saved.");
 		break;
 		
 		case 7:
 		// exit  <=== closes program
+			System.out.println("Good-bye!");
 			return "quit";
 	}
 	return "";	
@@ -109,7 +147,10 @@ public class MovieMenu {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			if (movieFromFile[4].equalsIgnoreCase(status.RELEASED.toString())) {
+			//System.out.println(status.RECEIVED.toString());
+			//System.out.println(movieFromFile[4].trim());
+			if (movieFromFile[4].trim().equalsIgnoreCase(status.RELEASED.toString())) {
+				
 				showing.add(addMovie);
 			} else {
 				coming.addLast(addMovie);
@@ -121,22 +162,47 @@ public class MovieMenu {
 		
 	}
 	
-	public static void addMovie(Scanner inputScanner) throws ParseException{
-		String movieTitle, description;
+	public static void addMovie(ArrayList<Movie> showing, DoubleLinkedList<Movie> coming) throws ParseException{
+		Scanner inputScanner = new Scanner(System.in);
+		String movieTitle = "", description = "";
 		Date releaseDate, receivedDate;
 		String recDate, dateRel = null;	
+		int category;
+		
 		
 		System.out.println("Enter movie title:");
-		movieTitle = inputScanner.next();
+		
+		
+		movieTitle = inputScanner.nextLine();
+		
+		
 		System.out.println("Enter release date in Format MM/dd/yyyy:");
 		dateRel = inputScanner.nextLine();
+		
+		
 		releaseDate = new SimpleDateFormat("MM/dd/yyyy").parse(dateRel);
+		
 		System.out.println("Enter description:");
-		description = inputScanner.next();
+		
+		description =  inputScanner.nextLine();
+		
 		System.out.println("Enter recieved date");
 		System.out.println("Format: MM/dd/yyyy");
-		recDate = inputScanner.next();
+		
+		recDate = inputScanner.nextLine();
+		
 		receivedDate = new SimpleDateFormat("MM/dd/yyyy").parse(recDate);
+		
+		System.out.println("What category is the movie?\n1....Showing\n2....Coming");
+		category = inputScanner.nextInt();
+		if (category == 1) {
+			showing.add(new Movie(movieTitle, releaseDate, description, receivedDate));
+		} else {
+			coming.addFirst(new Movie(movieTitle, releaseDate, description, receivedDate));
+		}
+		
+		
+		
 	}
 	public static void writeMovieFile(ArrayList<Movie> showing, DoubleLinkedList<Movie> coming) throws IOException {
 		// Create output file
@@ -144,14 +210,14 @@ public class MovieMenu {
 		// Create print writer to output data to output file
 		PrintWriter writeOutput = new PrintWriter(outputFile);
 		
-		Iterator show_it = showing.iterator();
+		java.util.Iterator<Movie> show_it = showing.iterator();
 		ListIterator com_it = coming.iterator();
 		
 		while (show_it.hasNext()) {
-			writeOutput.print(show_it.next().toString() + "released");
+			writeOutput.print(show_it.next().toString() + "released\n");
 		}
 		while (com_it.hasNext()) {
-			writeOutput.print(com_it.next().toString() + "received");
+			writeOutput.print(com_it.next().toString() + "received\n");
 		}
 		
 		writeOutput.close();
@@ -182,5 +248,6 @@ public class MovieMenu {
 			}
 			userQuit = menu(userSelected, inputScanner, showing, coming);
 		}
+		inputScanner.close();
 	}
 }
